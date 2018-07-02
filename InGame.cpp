9768472ae219853fd5f8b77f5f35e14b6cb36538
Park Dong UK <phk09242@gmail.com>
2018-07-02 11:59:10 +0900

    플레이어 및 적 제작

--- InGame.cpp
+++ InGame.cpp
@@ -4,26 +4,57 @@
 
 void InGame::Activate()
 {
+	string temp[10][10] =
+	{
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" },
+		{ "бр", "бр", "бр", "бр" , "бр" ,"бр" ,"бр" ,"бр" , "бр", "бр" }
+	};
+	map = new Map();
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			map->map[i][j] = temp[i][j];
+
+	player = new Player("бс", Vector2(1, 1));
+	enemy = new Enemy("б▄", Vector2(5, 5));
+
 	
 }
 
 void InGame::Update()
 {
+	if (g_input->OnceKeyDown(VK_ESCAPE))
+	{
+		g_scene->Activate(sTitle);
+		return;
+	}
 }
 
 void InGame::Render()
 {
-	
-	for (short i = 0; i < 10; ++i)
+	map->map[player->pos.x][player->pos.y] = player->shape;
+	map->map[enemy->pos.x][enemy->pos.y] = enemy->shape;
+
+	for (int i = 0; i < 10; ++i)
 	{
 		int z = 0;
-		for (short j = 0; j < 20; j += 2)
-		{
-			g_system->WriteText(j + 30, i + 8, cMap[i][z++]);
-		}
+		for (int j = 0; j < 20; j += 2)
+			g_system->WriteText(j + 30, i + 8, map->map[i][z++].c_str());
 	}
+
+	
 }
 
 void InGame::Deactivate()
 {
+	SAFE_DELETE(map);
+	SAFE_DELETE(enemy);
+	SAFE_DELETE(player);
 }
--- InGame.h
+++ InGame.h
@@ -5,19 +5,67 @@ class InGame
 	: public BaseScene
 {
 private:
-	const char* cMap[10][10] =
+	struct Vector2
 	{
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" },
-		{ "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр", "бр" }
+		int x;
+		int y;
+
+		Vector2(int x = 0, int y = 0)
+			: x(x), y(y)
+		{}
 	};
+
+	struct Map
+	{
+		string** map;
+		Vector2 size;
+
+		Map(Vector2 size = Vector2(10, 10))
+			: size(size)
+		{
+			map = new string*[size.y];
+
+			for (int i = 0; i < size.y; ++i)
+			{
+				map[i] = new string[size.x];
+			}
+		}
+		~Map()
+		{
+			for (int i = 0; i < size.y; ++i)
+			{
+				SAFE_DELETE_ARRAY(map[i]);
+			}
+			SAFE_DELETE_ARRAY(map);
+		}
+	};
+
+	struct Player
+	{
+		string shape;
+		Vector2 pos;
+		int move;
+
+		Player(string shape, Vector2 startPos, int move = 1)
+			: shape(shape), pos(startPos), move(move)
+		{}
+	};
+
+	struct Enemy
+	{
+		string shape;
+		Vector2 pos;
+		int move;
+
+		Enemy(string shape, Vector2 startPos, int move = 1)
+			: shape(shape), pos(startPos), move(move)
+		{}
+	};
+
+private:
+	Map * map;
+	Player* player;
+	Enemy* enemy;
 
 public:
 	virtual void Activate() override;
